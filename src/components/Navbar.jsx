9a84fd65baaea164ce9251f2acd72e0e829bd7ec
Ascended300 <eduardofelix014@gmail.com>
2022-04-30 16:43:00 -0300

    Avance de boton y poco responsive + stilos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,43 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './Navbar.css'
 
 function Navbar() {
   const [click, setClick] = useState(false);
+
+  const [button, setButton] = useState(true);
 
   const handleCLick = () => setClick(!click);
 
   const closeMobileMenu = () => setClick(false);
 
+  const showButton = () => {
+    if(window.innerWidth <= 960){ 
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  });
+
+  window.addEventListener('resize', showButton);
+
   return (
     <>
         <nav className='navbar'>
             <div className='navbar-container'>
-                <Link to="/" className='navbar-logo'>
-                    TRVL <i className="fab fa-typo3" />
+                <Link to="/" className='navbar-logo' onClick = {closeMobileMenu}>
+                    TRVL 
+                    <i className="fab fa-typo3" />
                 </Link>
                 <div className='menu-icon' onClick={handleCLick}>
                   <i className={click ? 'fas fa-times ' : 'fas fa-bars'} />
                 </div>
-                <ul className={click ? 'nav-menu active' : 'nav menu'}>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                   <li className="nav-item">
                     <Link to = '/' className='nav-links' onClick={closeMobileMenu}>Home</Link>
                   </li>
@@ -32,6 +51,7 @@ function Navbar() {
                     <Link to = '/' className='nav-links-mobile' onClick={closeMobileMenu}>Sign Up</Link>
                   </li>
                 </ul>
+                {button && <Button buttonStyle = 'btn--outline'>SIGN UP</Button>}
             </div>
         </nav>
         
