68ec3d42bbe7f7d9df5d5ac22c763b72711b4935
Ascended300 <eduardofelix014@gmail.com>
2022-04-30 22:08:04 -0300

    Avances con video en home y home con responsive

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../App.js'
+import HeroSection from '../HeroSection.jsx'
+
+function Home () {
+    return (
+        <>
+            <HeroSection/>
+        </>
+    )
+}
+
+export default Home
