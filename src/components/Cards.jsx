0d4fa6c969c1c25295d34deda104a386b1e0579d
Ascended300 <eduardofelix014@gmail.com>
2022-05-26 19:23:26 -0300

    mod basicos

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,47 +3,48 @@ import Carditem from './Carditem.jsx'
 import './Cards.css'
 
 function Cards() {
+
   return (
     <div className='cards'>
-        <h1>Check out these EPIC Destinations!</h1>
-        <div className="cards__container">
-            <div className="cards__wrapper">
-                <ul className="cards__items">
-                    <Carditem
-                    src='images/img-9.jpg' 
-                    text="Explore the hidden waterfall deep inside the Amazon Jungle"  
-                    label='Adventure' 
-                    path='/services'
-                    />
-                    <Carditem
-                    src='images/img-2.jpg' 
-                    text="Travel through the Island of Bali in a Private Cruise"  
-                    label='Adventure' 
-                    path='/services'
-                    />
-                </ul>
-                <ul className='cards__items'>
-                    <Carditem
-                      src='images/img-3.jpg'
-                      text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-                      label='Mystery'
-                      path='/services'
-                    />
-                    <Carditem
-                      src='images/img-4.jpg'
-                      text='Experience Football on Top of the Himilayan Mountains'
-                      label='Adventure'
-                      path='/products'
-                    />
-                    <Carditem
-                      src='images/img-8.jpg'
-                      text='Ride through the Sahara Desert on a guided camel tour'
-                      label='Adrenaline'
-                      path='/sign-up'
-                    />
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <Carditem
+              src='images/img-9.jpg'
+              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              label='Adventure'
+              path='/services'
+            />
+            <Carditem
+              src='images/img-2.jpg'
+              text="Travel through the Island of Bali in a Private Cruise"
+              label='Adventure'
+              path='/services'
+            />
           </ul>
-            </div>
+          <ul className='cards__items'>
+            <Carditem
+              src='images/img-3.jpg'
+              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+              label='Mystery'
+              path='/services'
+            />
+            <Carditem
+              src='images/img-4.jpg'
+              text='Experience Football on Top of the Himilayan Mountains'
+              label='Adventure'
+              path='/products'
+            />
+            <Carditem
+              src='images/img-8.jpg'
+              text='Ride through the Sahara Desert on a guided camel tour'
+              label='Adrenaline'
+              path='/sign-up'
+            />
+          </ul>
         </div>
+      </div>
     </div>
   )
 }
